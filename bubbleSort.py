521b600b072708d0fe4c172cf9afbbf537af9a42
ishaangupta122 <ishaang2209@gmail.com>
2025-06-13 11:29:24 +0530

    added bubble sort

---
+++ bubbleSort.py
@@ -0,0 +1,16 @@
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+arr = [5, 4, 3, 2, 1]
+print(bubbleSort(arr))
+
+
+
+
+
+
+
